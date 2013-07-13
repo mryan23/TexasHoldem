5700fdd94db01fb7a3c3158f4ee2a7913e578ef7
Chinmay Ghotkar <chinmayg@vt.edu>
2013-07-13 04:24:51 -0700

    Added Poker Files and Edited the Xml UI files

---
+++ PokerGame/src/com/example/poker/OutOfMoneyException.java
@@ -0,0 +1,11 @@
+package com.example.poker;
+
+public class OutOfMoneyException extends Exception {
+
+	private static final long serialVersionUID = -7343871194292515333L;
+
+	public String getMessage() {
+		return "Player is out of money";
+	}
+	
+}
