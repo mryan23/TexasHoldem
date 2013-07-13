196d92ed9c425b091ee60e3c72aeff3ebc51ebfb
Chinmay Ghotkar <chinmayg@vt.edu>
2013-07-13 09:08:12 -0700

    Added better ui to players hand

--- PokerGame/src/com/example/pokergame/MessageType.java
+++
@@ -1,7 +0,0 @@
-package com.example.pokergame;
-
-public enum MessageType {
-	JOIN, START, 				/* INIT */
-	CARD, RESULT, TURN,			/* DEALER */
-	BET, FOLD;			/* PLAYER */
-}
--- PokerGame/src/com/example/pokergame/Result.java
+++
@@ -1,5 +0,0 @@
-package com.example.pokergame;
-
-public enum Result {
-	WIN, LOSE, DRAW;
-}
