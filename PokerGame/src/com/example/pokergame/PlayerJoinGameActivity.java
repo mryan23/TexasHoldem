a5f12672e0f697bfdf98910bf09a83d947578d18
Sagar <smedikeri@cmu.edu>
2013-07-13 03:27:08 -0700

    join/startgame

--- PokerGame/src/com/example/pokergame/PlayerJoinGameActivity.java
+++ PokerGame/src/com/example/pokergame/PlayerJoinGameActivity.java
@@ -2,12 +2,19 @@ package com.example.pokergame;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,35 +27,65 @@ public class PlayerJoinGameActivity extends Activity {
 
 	EditText ipAddressField;
 	Button joinButton;
+	ArrayList<Integer> isSuccessful = new ArrayList<Integer>();
+	Context context = this;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_player_join_game);
-		ipAddressField = (EditText)findViewById(R.id.ipAddressEditText);
+		ipAddressField = (EditText) findViewById(R.id.ipAddressEditText);
 		joinButton = (Button) findViewById(R.id.joinGameButton);
-		setListeners();
+
+		TCPListener listener;
+
+		try {
+			listener = new TCPListener(new ServerSocket(6789), isSuccessful);
+			listener.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setListeners(); // send ip/joining msg
+
+		Timer autoUpdate = new Timer();
+		autoUpdate.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					public void run() {
+						if (isSuccessful.contains(1)) {
+							System.out.println("Value in Activity: " + isSuccessful.get(0));
+							Intent i = new Intent(context, PlayerHandActivity.class);
+							startActivity(i);
+						}
+					}
+				});
+			}
+		}, 0, 100);
+
 	}
-	
-	private void setListeners(){
-		joinButton.setOnClickListener(new OnClickListener(){
+
+	private void setListeners() {
+		joinButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
+
 				String str = ipAddressField.getText().toString();
 				try {
-					SendTcpMessage stm = new SendTcpMessage(InetAddress.getByName(str),"TESTING123");
+					SendTcpMessage stm = new SendTcpMessage(InetAddress
+							.getByName(str), "JOINING");
 					stm.start();
 				} catch (UnknownHostException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
-			
+
 		});
 	}
-
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- PokerGame/src/com/example/pokergame/TCPListener.java
+++ PokerGame/src/com/example/pokergame/TCPListener.java
@@ -2,6 +2,7 @@ package com.example.pokergame;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -13,6 +14,15 @@ public class TCPListener extends Thread {
 
 	ArrayList<InetAddress> player_addr;
 	Object lock;
+	ArrayList<Integer> isSuccessful;
+	boolean loop = true;
+	boolean isDealer = true;
+
+	public TCPListener(ServerSocket socket, ArrayList<Integer> isSuccessful) {
+		sock = socket;
+		this.isSuccessful = isSuccessful;
+		isDealer = false;
+	}
 
 	public TCPListener(ServerSocket socket, ArrayList<InetAddress> player_addr,
 			Object lock) {
@@ -22,8 +32,14 @@ public class TCPListener extends Thread {
 	}
 
 	public void run() {
-		while (true)
+		while (loop)
 			tcpStuff(sock);
+		try {
+			sock.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void tcpStuff(ServerSocket welcomeSocket) {
@@ -38,17 +54,24 @@ public class TCPListener extends Thread {
 				message = (bytes[0] & 0xff) + "." + (bytes[1] & 0xff) + "."
 						+ (bytes[2] & 0xff) + "." + (bytes[3] & 0xff);
 			}
-
-			InetAddress ip = InetAddress.getByName(message);
-			synchronized (lock) {
-				if (!this.player_addr.contains(ip)) {
-					System.out.println(message);
-					this.player_addr.add(ip);
+			if (isDealer) {
+				InetAddress ip = InetAddress.getByName(message);
+				synchronized (lock) {
+					if (!this.player_addr.contains(ip)) {
+						System.out.println(message);
+						this.player_addr.add(ip);
+					}
 				}
+			}
+			String clientSentence = inFromClient.readLine();
+			System.out.println(clientSentence);
+			if (clientSentence.contains("STARTGAME")) {
+				isSuccessful.add(1);
+				System.out.println("Value: " + isSuccessful.get(0));
+				loop = false;
 			}
 			DataOutputStream outToClient = new DataOutputStream(
 					connectionSocket.getOutputStream());
-			String clientSentence = inFromClient.readLine();
 			outToClient.writeBytes(clientSentence + '\n');
 			// System.out.println(clientSentence);
 			// message = clientSentence;
